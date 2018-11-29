339ebc74065fbd6bc39713e75e8ba76a63fb9637
Erin Millard <ezzatron@gmail.com>
2018-11-29 20:13:34 +1000

    Implement ES2015 module index for sound modules

---
+++ src/sound/index-es.js
@@ -0,0 +1,21 @@
+import BaseSound from './BaseSound'
+import BaseSoundManager from './BaseSoundManager'
+import HTML5AudioSound from './html5/HTML5AudioSound'
+import HTML5AudioSoundManager from './html5/HTML5AudioSoundManager'
+import NoAudioSound from './noaudio/NoAudioSound'
+import NoAudioSoundManager from './noaudio/NoAudioSoundManager'
+import SoundManagerCreator from './SoundManagerCreator'
+import WebAudioSound from './webaudio/WebAudioSound'
+import WebAudioSoundManager from './webaudio/WebAudioSoundManager'
+
+export {
+    BaseSound,
+    BaseSoundManager,
+    HTML5AudioSound,
+    HTML5AudioSoundManager,
+    NoAudioSound,
+    NoAudioSoundManager,
+    SoundManagerCreator,
+    WebAudioSound,
+    WebAudioSoundManager,
+}
