a37a95eda1566d7e78a2f20f101810836c1e2cc7
Erin Millard <ezzatron@gmail.com>
2018-11-29 20:19:06 +1000

    Implement ES2015 module indexes for texture modules

---
+++ src/textures/index-es.js
@@ -0,0 +1,22 @@
+import Frame from './Frame'
+import Parsers from './parsers'
+import Texture from './Texture'
+import TextureManager from './TextureManager'
+import TextureSource from './TextureSource'
+
+import FilterMode, {
+    LINEAR,
+    NEAREST,
+} from './const'
+
+export {
+    FilterMode,
+    Frame,
+    Parsers,
+    Texture,
+    TextureManager,
+    TextureSource,
+
+    LINEAR,
+    NEAREST,
+}
---
+++ src/textures/parsers/index-es.js
@@ -0,0 +1,19 @@
+import AtlasXML from './AtlasXML'
+import Canvas from './Canvas'
+import Image from './Image'
+import JSONArray from './JSONArray'
+import JSONHash from './JSONHash'
+import SpriteSheet from './SpriteSheet'
+import SpriteSheetFromAtlas from './SpriteSheetFromAtlas'
+import UnityYAML from './UnityYAML'
+
+export {
+    AtlasXML,
+    Canvas,
+    Image,
+    JSONArray,
+    JSONHash,
+    SpriteSheet,
+    SpriteSheetFromAtlas,
+    UnityYAML,
+}
