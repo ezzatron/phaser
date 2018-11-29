15409f8547980dcd234e340d59336c21a2ce2a56
Erin Millard <ezzatron@gmail.com>
2018-11-29 20:10:34 +1000

    Implement ES2015 module index for scene modules

---
+++ src/scene/index-es.js
@@ -0,0 +1,35 @@
+import SceneManager from './SceneManager'
+import ScenePlugin from './ScenePlugin'
+import Settings from './Settings'
+import Systems from './Systems'
+
+import {
+    PENDING,
+    INIT,
+    START,
+    LOADING,
+    CREATING,
+    RUNNING,
+    PAUSED,
+    SLEEPING,
+    SHUTDOWN,
+    DESTROYED,
+} from './const'
+
+export {
+    SceneManager,
+    ScenePlugin,
+    Settings,
+    Systems,
+
+    PENDING,
+    INIT,
+    START,
+    LOADING,
+    CREATING,
+    RUNNING,
+    PAUSED,
+    SLEEPING,
+    SHUTDOWN,
+    DESTROYED,
+}
