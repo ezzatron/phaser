4798cbf59e9b1fdc08665fe64637d90c7a393028
Erin Millard <ezzatron@gmail.com>
2018-11-29 20:36:55 +1000

    Implement ES2015 module indexes for tween modules

---
+++ src/tweens/builders/index-es.js
@@ -0,0 +1,23 @@
+import GetBoolean from './GetBoolean'
+import GetEaseFunction from './GetEaseFunction'
+import GetNewValue from './GetNewValue'
+import GetProps from './GetProps'
+import GetTargets from './GetTargets'
+import GetTweens from './GetTweens'
+import GetValueOp from './GetValueOp'
+import NumberTweenBuilder from './NumberTweenBuilder'
+import TimelineBuilder from './TimelineBuilder'
+import TweenBuilder from './TweenBuilder'
+
+export {
+    GetBoolean,
+    GetEaseFunction,
+    GetNewValue,
+    GetProps,
+    GetTargets,
+    GetTweens,
+    GetValueOp,
+    NumberTweenBuilder,
+    TimelineBuilder,
+    TweenBuilder,
+}
---
+++ src/tweens/index-es.js
@@ -0,0 +1,51 @@
+import Builders from './builders/index-es'
+import TweenManager from './TweenManager'
+import Tween from './tween/Tween'
+import TweenData from './tween/TweenData'
+import Timeline from './Timeline'
+
+import {
+    CREATED,
+    INIT,
+    DELAY,
+    OFFSET_DELAY,
+    PENDING_RENDER,
+    PLAYING_FORWARD,
+    PLAYING_BACKWARD,
+    HOLD_DELAY,
+    REPEAT_DELAY,
+    COMPLETE,
+    PENDING_ADD,
+    PAUSED,
+    LOOP_DELAY,
+    ACTIVE,
+    COMPLETE_DELAY,
+    PENDING_REMOVE,
+    REMOVED,
+} from './tween/const'
+
+export {
+    Builders,
+    TweenManager,
+    Tween,
+    TweenData,
+    Timeline,
+
+    CREATED,
+    INIT,
+    DELAY,
+    OFFSET_DELAY,
+    PENDING_RENDER,
+    PLAYING_FORWARD,
+    PLAYING_BACKWARD,
+    HOLD_DELAY,
+    REPEAT_DELAY,
+    COMPLETE,
+    PENDING_ADD,
+    PAUSED,
+    LOOP_DELAY,
+    ACTIVE,
+    COMPLETE_DELAY,
+    PENDING_REMOVE,
+    REMOVED,
+}
