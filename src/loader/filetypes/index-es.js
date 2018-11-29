2c08545cb7d51c0f9bed6321471180320334afd6
Erin Millard <ezzatron@gmail.com>
2018-11-29 19:12:20 +1000

    Implement ES2015 module indexes for loader modules

---
+++ src/loader/filetypes/index-es.js
@@ -0,0 +1,55 @@
+import AnimationJSONFile from './AnimationJSONFile'
+import AtlasJSONFile from './AtlasJSONFile'
+import AtlasXMLFile from './AtlasXMLFile'
+import AudioFile from './AudioFile'
+import AudioSpriteFile from './AudioSpriteFile'
+import BinaryFile from './BinaryFile'
+import BitmapFontFile from './BitmapFontFile'
+import GLSLFile from './GLSLFile'
+import HTML5AudioFile from './HTML5AudioFile'
+import HTMLFile from './HTMLFile'
+import HTMLTextureFile from './HTMLTextureFile'
+import ImageFile from './ImageFile'
+import JSONFile from './JSONFile'
+import MultiAtlasFile from './MultiAtlasFile'
+import PackFile from './PackFile'
+import PluginFile from './PluginFile'
+import ScenePluginFile from './ScenePluginFile'
+import ScriptFile from './ScriptFile'
+import SpriteSheetFile from './SpriteSheetFile'
+import SVGFile from './SVGFile'
+import TextFile from './TextFile'
+import TilemapCSVFile from './TilemapCSVFile'
+import TilemapImpactFile from './TilemapImpactFile'
+import TilemapJSONFile from './TilemapJSONFile'
+import UnityAtlasFile from './UnityAtlasFile'
+import XMLFile from './XMLFile'
+
+export {
+    AnimationJSONFile,
+    AtlasJSONFile,
+    AtlasXMLFile,
+    AudioFile,
+    AudioSpriteFile,
+    BinaryFile,
+    BitmapFontFile,
+    GLSLFile,
+    HTML5AudioFile,
+    HTMLFile,
+    HTMLTextureFile,
+    ImageFile,
+    JSONFile,
+    MultiAtlasFile,
+    PackFile,
+    PluginFile,
+    ScenePluginFile,
+    ScriptFile,
+    SpriteSheetFile,
+    SVGFile,
+    TextFile,
+    TilemapCSVFile,
+    TilemapImpactFile,
+    TilemapJSONFile,
+    UnityAtlasFile,
+    XMLFile,
+}
---
+++ src/loader/index-es.js
@@ -0,0 +1,55 @@
+import File from './File'
+import FileTypes from './filetypes/index-es'
+import FileTypesManager from './FileTypesManager'
+import GetURL from './GetURL'
+import LoaderPlugin from './LoaderPlugin'
+import MergeXHRSettings from './MergeXHRSettings'
+import MultiFile from './MultiFile'
+import XHRLoader from './XHRLoader'
+import XHRSettings from './XHRSettings'
+
+import {
+    LOADER_IDLE,
+    LOADER_LOADING,
+    LOADER_PROCESSING,
+    LOADER_COMPLETE,
+    LOADER_SHUTDOWN,
+    LOADER_DESTROYED,
+    FILE_PENDING,
+    FILE_LOADING,
+    FILE_LOADED,
+    FILE_FAILED,
+    FILE_PROCESSING,
+    FILE_ERRORED,
+    FILE_COMPLETE,
+    FILE_DESTROYED,
+    FILE_POPULATED,
+} from './const'
+
+export {
+    File,
+    FileTypes,
+    FileTypesManager,
+    GetURL,
+    LoaderPlugin,
+    MergeXHRSettings,
+    MultiFile,
+    XHRLoader,
+    XHRSettings,
+
+    LOADER_IDLE,
+    LOADER_LOADING,
+    LOADER_PROCESSING,
+    LOADER_COMPLETE,
+    LOADER_SHUTDOWN,
+    LOADER_DESTROYED,
+    FILE_PENDING,
+    FILE_LOADING,
+    FILE_LOADED,
+    FILE_FAILED,
+    FILE_PROCESSING,
+    FILE_ERRORED,
+    FILE_COMPLETE,
+    FILE_DESTROYED,
+    FILE_POPULATED,
+}
