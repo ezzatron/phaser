d8d2fe6046824879d5fa8529ba9ee144b2b65a18
Erin Millard <ezzatron@gmail.com>
2018-11-29 19:57:08 +1000

    Implement ES2015 module index for plugin modules

---
+++ src/plugins/index-es.js
@@ -0,0 +1,13 @@
+import BasePlugin from './BasePlugin'
+import DefaultPlugins from './DefaultPlugins'
+import PluginCache from './PluginCache'
+import PluginManager from './PluginManager'
+import ScenePlugin from './ScenePlugin'
+
+export {
+    BasePlugin,
+    DefaultPlugins,
+    PluginCache,
+    PluginManager,
+    ScenePlugin,
+}
