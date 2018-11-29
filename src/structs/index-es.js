36f9befb40538faec8a47f7ddde68c4917756686
Erin Millard <ezzatron@gmail.com>
2018-11-29 20:15:12 +1000

    Implement ES2015 module index for struct modules

---
+++ src/structs/index-es.js
@@ -0,0 +1,13 @@
+import List from './List')
+import Map from './Map')
+import ProcessQueue from './ProcessQueue')
+import RTree from './RTree')
+import Set from './Set')
+
+export {
+    List,
+    Map,
+    ProcessQueue,
+    RTree,
+    Set,
+}
